ad900090501294e416035bdba7c6df731a8adb1d
Tate Rathje <Taterathje@gmail.com>
2013-10-10 23:17:57 -0700

    Updated profile and added scoped status updates to it

--- test/integration/custom_routes_test.rb
+++ test/integration/custom_routes_test.rb
@@ -16,4 +16,9 @@ class CustomRoutesTest < ActionDispatch::IntegrationTest
     get '/register'
     assert_response :success	
   end
+
+  test "that a profile page works" do
+    get '/Flint285'
+    assert_response :success
+  end
 end
