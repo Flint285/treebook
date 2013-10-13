50fd4d37ce82f543580a51d34f5162f6084ac1ad
Tate Rathje <Taterathje@gmail.com>
2013-10-13 03:12:43 -0700

    Scoped status updates to the current user on create and update. Also, had 1 failure and 1 errors in statuses_controller_test that I could not resolve.

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -39,6 +39,17 @@ end
     assert_redirected_to status_path(assigns(:status))
   end
 
+  test "should create status for the current user when logged in" do
+    sign_in users(:tate)
+
+    assert_difference('Status.count') do
+      post :create, status: { content: @status.content, user_id: users(:bill).id }
+    end
+
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:tate).id
+  end
+
   test "should show status" do
     get :show, id: @status
     assert_response :success
@@ -70,6 +81,20 @@ end
     sign_in users(:tate)
     put :update, id: @status, status: { content: @status.content }
     assert_redirected_to status_path(assigns(:status))
+  end
+
+  test "should update status for the current user when logged in" do
+    sign_in users(:tate)
+    put :update, id: @status, status: { content: @status.content, user_id: users(:bill).id }
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:tate).id
+  end
+
+  test "should not update the status if nothing has changed" do
+    sign_in users(:tate)
+    put :update, id: @status
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:tate).id
   end
 
   test "should destroy status" do
